fb7e47697e448e9950f3772a4ed76a4a7c8893d3
Sergei Iakhnin <llevar@gmail.com>
2018-04-18 18:08:12 +0200

    Remove null resource

--- deployment/hd/worker.tf
+++ deployment/hd/worker.tf
@@ -36,20 +36,6 @@ resource "openstack_compute_instance_v2" "worker" {
 		delete_on_termination = true
 	}*/
 
-	count = "${var.worker_count}"
-	key_pair = "${var.key_pair}"
-}
-
-resource "null_resource" "worker" {
-	depends_on = ["openstack_compute_instance_v2.worker"]
-	connection {
-		user = "${var.user}"
-		private_key = "${file(var.key_file)}"
-		bastion_private_key = "${file(var.key_file)}"
-		bastion_host = "${var.bastion_host}"
-		bastion_user = "${var.bastion_user}"
-		agent = true
-	}
 	provisioner "file" {
 		source = "salt_setup.sh"
 		destination = "/tmp/salt_setup.sh"	
@@ -60,4 +46,7 @@ resource "null_resource" "worker" {
 		"/tmp/salt_setup.sh ${null_resource.masterip.triggers.address} worker-${count.index} \"worker, consul-client, ebi\""
 		]
 	}
+	
+	count = "${var.worker_count}"
+	key_pair = "${var.key_pair}"
 }
